2b0682d7d513f2edcdb21574b35dba90e1eabae4
Ezra Szanton <ezraszanton@yahoo.com>
2020-07-11 10:50:23 -0400

    Added some music and imported sounds

---
+++ Assets/VaryVolumeSin.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(AudioSource))]
+public class VaryVolumeSin : MonoBehaviour
+{
+    private AudioSource music;
+
+    private float maxVolume;
+
+    public float period;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        music = GetComponent<AudioSource>();
+        maxVolume = music.volume;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        music.volume = ( Mathf.Sin(2 * Mathf.PI * Time.time / period) + 1) * maxVolume / 2;
+    }
+}
