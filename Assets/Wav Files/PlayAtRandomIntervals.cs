553f7e04d787d3dfcecd09591f4b6efbe8853d5c
Ezra Szanton <ezraszanton@yahoo.com>
2020-07-11 17:56:12 -0400

    added audio ambiance

---
+++ Assets/Wav Files/PlayAtRandomIntervals.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class PlayAtRandomIntervals : MonoBehaviour
+{
+    public List<AudioClip> sounds;
+
+    public float minTime, maxTime;
+
+    float nextTime = 0;
+
+    private AudioSource sound;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sound = GetComponent<AudioSource>();
+
+        nextTime += Random.Range(minTime, maxTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!sound.isPlaying && Time.time > nextTime)
+        {
+            nextTime += Random.Range(minTime, maxTime);
+
+            sound.clip = sounds[Random.Range(0, sounds.Count)];
+
+            sound.Play();
+        }
+    }
+}
