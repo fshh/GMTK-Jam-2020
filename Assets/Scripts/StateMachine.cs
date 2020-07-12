fc06e4164fb4225cf62897fe131617b7d308498e
Ezra Szanton <ezraszanton@yahoo.com>
2020-07-11 23:03:12 -0400

    implemented main state logic and added VO

---
+++ Assets/Scripts/StateMachine.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StateMachine : MonoBehaviour
+{
+    public enum state { User, Choosing, Voice, Typing};
+
+    public state gameState;
+
+    bool playingCoroutine = false;
+
+    public TSVParser parser;
+
+    public int line = 0;
+
+    public float secPerLetter;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (gameState)
+        {
+            case state.User:
+                if (Input.GetKeyDown(KeyCode.Return))
+                {
+                    gameState = state.Choosing;
+                }
+                break;
+            case state.Choosing:
+                if (parser.isSpoken[line])
+                {
+                    gameState = state.Voice;
+                } else
+                {
+                    gameState = state.Typing;
+                }
+                break;
+            case state.Voice:
+                if (!playingCoroutine)
+                {
+                    StartCoroutine(playVO());
+                }
+                break;
+            case state.Typing:
+                if (!playingCoroutine)
+                {
+                    StartCoroutine(pastingText());
+                }
+                break;
+        }
+
+    }
+
+    IEnumerator playVO()
+    {
+        playingCoroutine = true;
+        yield return new WaitForSeconds(1);
+        gameState = state.Choosing;
+        playingCoroutine = false;
+        line++;
+    }
+
+    IEnumerator pastingText()
+    {
+        playingCoroutine = true;
+        yield return new WaitForSeconds(secPerLetter * parser.wordList[line].Length);
+        parser.addText(line);
+        gameState = state.User;
+        playingCoroutine = false;
+        line++;
+    }
+    
+}
