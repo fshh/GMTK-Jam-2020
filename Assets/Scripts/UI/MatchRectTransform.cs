bef2ef4b21f9d5c4394778b6bb358d06b40b91e7
fshh <ajknollmeyer@gmail.com>
2020-08-08 16:02:33 -0400

    Highlight scrolls with text, 'typing...' scrolls with text, fix scrollbar placement

---
+++ Assets/Scripts/UI/MatchRectTransform.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MatchRectTransform : MonoBehaviour
+{
+    public RectTransform reference;
+
+    private RectTransform rectTransform;
+
+    private void Awake()
+    {
+        rectTransform = GetComponent<RectTransform>();
+    }
+
+    private void Update()
+    {
+        // System.Reflection.FieldInfo[] fields = reference.GetType().GetFields();
+        // foreach (System.Reflection.FieldInfo field in fields)
+        // {
+        //     field.SetValue(rectTransform, field.GetValue(reference));
+        // }
+
+        // rectTransform.anchorMax = reference.anchorMax;
+        // rectTransform.anchorMin = reference.anchorMin;
+        // rectTransform.pivot = reference.pivot;
+        // rectTransform.offsetMax = reference.offsetMax;
+        // rectTransform.offsetMin = reference.offsetMin;
+        // rectTransform.anchoredPosition3D = reference.anchoredPosition3D;
+
+        rectTransform.pivot = reference.pivot;
+        rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, reference.rect.width);
+        rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, reference.rect.height);
+        rectTransform.position = reference.position;
+    }
+}
