fb406f89b6ab76ebde9a249d8886ca2b104b20c4
Andrew Knollmeyer <ajknollmeyer@gmail.com>
2021-01-01 11:57:56 -0800

    Adjust volume & frequency of background noises, add button to play them in inspector

---
+++ Assets/Editor/PlayAtRandomIntervalsEditor.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(PlayAtRandomIntervals))]
+public class PlayAtRandomIntervalsEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        PlayAtRandomIntervals myScript = (PlayAtRandomIntervals)target;
+        if (GUILayout.Button("Play Sound"))
+        {
+            myScript.PlaySound();
+        }
+    }
+}
