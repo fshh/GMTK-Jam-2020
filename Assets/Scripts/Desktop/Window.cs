4cde64aad707e04cec06db0a6b7edcdec68e3382
Andrew Knollmeyer <ajknollmeyer@gmail.com>
2021-05-12 15:51:36 -0400

    Fullscreen & un-fullscreen

--- Assets/Scripts/Desktop/Window.cs
+++ Assets/Scripts/Desktop/Window.cs
@@ -3,18 +3,35 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 [RequireComponent(typeof(CanvasGroup))]
 public class Window : MonoBehaviour, IPointerClickHandler
 {
 	public RectTransform ContentParent;
 	public TextMeshProUGUI TitleText;
+	public Button FullscreenButton;
+	public Button UnFullscreenButton;
+	public DragTarget WindowDragger;
 	public ApplicationSO App { get; private set; }
 	public bool Minimized { get => canvasGroup.alpha == 0f; }
 
 	private GameObject content;
 	private SaveDataGeneric<GameObject> save;
 	private CanvasGroup canvasGroup;
+
+	private class WindowPosAndSize
+    {
+		public readonly Vector2 AnchoredPos;
+		public readonly Vector2 SizeDelta;
+
+		public WindowPosAndSize(Vector2 anchoredPos, Vector2 sizeDelta)
+        {
+			AnchoredPos = anchoredPos;
+			SizeDelta = sizeDelta;
+        }
+    }
+	private WindowPosAndSize prevPosAndSize;
 
     private void Awake()
     {
@@ -78,12 +95,27 @@ public class Window : MonoBehaviour, IPointerClickHandler
 
 	public void Fullscreen()
 	{
-		throw new System.NotImplementedException();
+		RectTransform rectTransform = GetComponent<RectTransform>();
+		prevPosAndSize = new WindowPosAndSize(rectTransform.anchoredPosition, rectTransform.sizeDelta);
+		rectTransform.anchorMin = Vector2.zero;
+		rectTransform.anchorMax = Vector2.one;
+		rectTransform.anchoredPosition = Vector2.zero;
+		rectTransform.sizeDelta = Vector2.zero;
+		FullscreenButton.gameObject.SetActive(false);
+		UnFullscreenButton.gameObject.SetActive(true);
+		WindowDragger.gameObject.SetActive(false);
 	}
 
 	public void UnFullscreen()
 	{
-		throw new System.NotImplementedException();
+		RectTransform rectTransform = GetComponent<RectTransform>();
+		rectTransform.anchorMin = new Vector2(0.5f, 0.5f);
+		rectTransform.anchorMax = new Vector2(0.5f, 0.5f);
+		rectTransform.anchoredPosition = prevPosAndSize.AnchoredPos;
+		rectTransform.sizeDelta = prevPosAndSize.SizeDelta;
+		FullscreenButton.gameObject.SetActive(true);
+		UnFullscreenButton.gameObject.SetActive(false);
+		WindowDragger.gameObject.SetActive(true);
 	}
 
 	public void Save()
