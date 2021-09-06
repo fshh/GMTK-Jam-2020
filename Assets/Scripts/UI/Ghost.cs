190afcca0832d400a0255b2508d1085b80b65ff1
Ezra Szanton <ezraszanton@yahoo.com>
2021-09-06 16:08:44 -0400

    Added cursor effect

---
+++ Assets/Scripts/UI/Ghost.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Ghost : MonoBehaviour
+{
+    private GameObject parentOb;
+    private GhostChild[] ghosties;
+    [Range(1,100)]
+    public int numGhosties, currGhosty;
+    [Range(0.0001f,5f)]
+    public float waitTime;
+    public GameObject ghostPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        parentOb = new GameObject();
+        parentOb.AddComponent<RectTransform>();
+        parentOb.GetComponent<RectTransform>().SetParent(transform.parent);
+        GetComponent<RectTransform>().SetParent(parentOb.transform);
+        parentOb.transform.localScale = Vector3.one;
+        parentOb.name = "Cursor Ghost Parent (Created at runtime)";
+        transform.localScale = Vector3.one;
+
+        ghosties = new GhostChild[numGhosties];
+        for (int i = 0; i < numGhosties; i++)
+        {
+            GameObject ghost = Instantiate(ghostPrefab, parentOb.transform);
+            ghosties[i] = ghost.GetComponent<GhostChild>();
+        }
+
+        StartCoroutine(ManageGhosts());
+    }
+
+    private IEnumerator ManageGhosts()
+    {
+        while (true)
+        {
+            ghosties[currGhosty].transform.SetAsLastSibling();
+            transform.SetAsLastSibling();
+            ghosties[currGhosty].SetPosition(GetComponent<RectTransform>());
+            yield return new WaitForSeconds(waitTime);
+            currGhosty = (currGhosty + 1) % numGhosties;
+        }
+    }
+}
---
+++ Assets/Scripts/UI/GhostChild.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GhostChild : MonoBehaviour
+{
+    public Image image;
+    public void SetPosition(RectTransform t)
+    {
+        Color temp = image.color;
+        temp.a = 1;
+        image.color = temp;
+        GetComponent<RectTransform>().position = t.position;
+        image.DOFade(0, 1);
+    }
+}
