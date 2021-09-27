11741d34ee91d0b3503bc8586f8e626b63b52ce4
fshh <ajknollmeyer@gmail.com>
2021-09-27 19:09:33 -0400

    3d and 2d scenes

---
+++ Assets/Scripts/UI/ContainObjectWithinRect.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(RectTransform))]
+public class ContainObjectWithinRect : MonoBehaviour
+{
+    public RectTransform ObjectToContain;
+
+    private RectTransform rectTransform;
+
+    private void Awake()
+    {
+        rectTransform = GetComponent<RectTransform>();
+    }
+
+    private void LateUpdate()
+    {
+        Rect rect = rectTransform.rect;
+
+        float leftSide = rectTransform.anchoredPosition.x - rect.width / 2;
+        float rightSide = rectTransform.anchoredPosition.x + rect.width / 2;
+        float topSide = rectTransform.anchoredPosition.y + rect.height / 2;
+        float bottomSide = rectTransform.anchoredPosition.y - rect.height / 2;
+
+        ObjectToContain.anchoredPosition = new Vector2(
+            Mathf.Clamp(ObjectToContain.anchoredPosition.x, leftSide, rightSide), 
+            Mathf.Clamp(ObjectToContain.anchoredPosition.y, bottomSide, topSide));
+    }
+}
