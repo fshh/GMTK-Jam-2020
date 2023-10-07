428c4baaebe951380b87cc7de3c8a74972b45919
fshh <ajknollmeyer@gmail.com>
2023-10-07 16:37:26 -0400

    Add model to make Clarity mesh disappearing work, clamp 3D camera movement

--- Assets/Scripts/Misc/Camera1stPerson.cs
+++ Assets/Scripts/Misc/Camera1stPerson.cs
@@ -6,11 +6,15 @@ public class Camera1stPerson : MonoBehaviour
 {
     Vector2 rotation = Vector2.zero;
     public float speed = 3;
+    public float maxRotationX = 80;
+    public float maxRotationY = 80;
 
     void Update()
     {
-        rotation.y += Input.GetAxis("Mouse X");
-        rotation.x += -Input.GetAxis("Mouse Y");
-        transform.eulerAngles = (Vector2)rotation * speed;
+        rotation.y += Input.GetAxis("Mouse X") * speed;
+        rotation.x += -Input.GetAxis("Mouse Y") * speed;
+        rotation.y = Mathf.Clamp(rotation.y, -maxRotationY, maxRotationY);
+        rotation.x = Mathf.Clamp(rotation.x, -maxRotationX, maxRotationX);
+        transform.eulerAngles = rotation;
     }
 }
